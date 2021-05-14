65c5640dd8dc7c6dfa96f171f3a3f18b0def9c68
omegion <kurtulus@mail.com>
2021-05-14 14:18:28 +0200

    Refactor

--- examples/use-modules/main.tf
+++ examples/use-modules/main.tf
@@ -4,16 +4,11 @@ provider "aws" {
 }
 
 module "module_1" {
-  source   = "git@github.com:omegion/terraform-modules.git//modules/module-1?ref=module-1-v0.1.0"
+  source   = "git@github.com:omegion/terraform-modules.git//modules/module-1?ref=module-1-v0.2.0"
   variable = "module-variable-1"
 }
 
 module "module_2" {
-  source   = "git@github.com:omegion/terraform-modules.git//modules/module-2?ref=module-2-v0.4.0"
+  source   = "git@github.com:omegion/terraform-modules.git//modules/module-2?ref=module-2-v0.5.0"
   variable = "module-variable-2"
 }
-
-//module "module_3" {
-//  source  = "example.com/mycompany/foo-service/aws"
-//  version = "1.0.0"
-//}
