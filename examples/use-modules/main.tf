e13625aa4fc667541d5e8d79a5c9d7fc23207e66
omegion <kurtulus@mail.com>
2021-05-17 10:50:59 +0200

    add more explanations

--- examples/use-modules/main.tf
+++ examples/use-modules/main.tf
@@ -4,11 +4,6 @@ provider "aws" {
 }
 
 module "module_1" {
-  source   = "git@github.com:omegion/terraform-modules.git//modules/module-1?ref=module-1-v0.2.0"
-  variable = "module-variable-1"
-}
-
-module "module_2" {
-  source   = "git@github.com:omegion/terraform-modules.git//modules/module-2?ref=module-2-v0.5.0"
-  variable = "module-variable-2"
+  source  = "registry.acquia.io/accounts"
+  version = "0.1.0"
 }
